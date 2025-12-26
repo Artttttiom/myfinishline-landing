c88387071f397f4734fadfe153aef7010fe1db41
Vladilen <45649644+vladilen597@users.noreply.github.com>
2025-12-27 00:25:33 +0300

    Update googleAuth.ts

--- app/lib/utils/googleAuth.ts
+++ app/lib/utils/googleAuth.ts
@@ -1,7 +1,7 @@
 export const googleAuthUrl = () => {
   const params = new URLSearchParams({
     client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
-    redirect_uri: "http://localhost:3000/auth/google/callback",
+    redirect_uri: "https://dev.myfinishline.io/auth/google/callback",
     response_type: "code",
     scope: "openid email profile",
     access_type: "offline",
